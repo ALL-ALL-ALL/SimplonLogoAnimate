114e1258c43965be6d600297c2d1e1f6c1430557
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-01 19:42:59 +0200

    Initial Commit

---
+++ SimplonLogoAnimate/SimplonLogoAnimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplonLogoAnimateApp.swift
+//  SimplonLogoAnimate
+//
+//  Created by  Ixart on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimplonLogoAnimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
